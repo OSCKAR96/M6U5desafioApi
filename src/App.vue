3be4dfbef08474edd3167228ed94df45321e0676
Oscar <oscarwebster96@gmail.com>
2024-10-23 17:56:53 -0300

    empezando desafio vue

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="p-3">
+    <h1 class="text-center">Centro de Mensajes</h1>
+
+    <div class="row container">
+
+      <div class="col-3 carta">
+        <CardUser/>
+      </div>
+
+      <div class="col-6 carta">
+        <h1>CardChat.vue</h1>
+      </div>
+
+      <div class="col-3 carta">
+        <h1>CardUsuario.vue</h1>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+import CardUser from "./components/CardUser.vue"
+import CardChat from "./components/CardChat.vue"
+
+export default {
+  name: "App",
+  components: {
+    CardChat,
+    CardUser,
+  },
+  data (){
+    return{
+      usuario:[],
+      mensaje:[],
+    };
+  },
+  methods:{
+    agregarChat(nuevoMsj){
+      this.mensajes.push(nuevoMsj);
+    }
+  },
+  async mounted(){
+    const response = await axios.get("https://randomuser.me/api/?results=2")
+    this.usuario = response.data.results;
+  }
+}
+</script>
+
+
+
+<style scoped>
+.carta {
+  border: black 1px solid;
+}
+
+</style>
---
+++ src/components/CardUser.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <div>
+    <div>
+      <img src="../../src/assets/logo.svg" alt="imagenUsuario" class="">
+    </div>
+    <div>
+      <span></span>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: "CardUser",
+  props: {
+    imagenUsuario: String,
+    nombreUsuario: String,
+    posicion: String,
+  },
+  data(){
+    return{
+      colorChat:"",
+      textoChat:"",
+    }
+  },
+  methods:{
+    enviarMsj: function(){
+      let mensajeChat = {
+        autor: this.nombreUsuario,
+        color: this.colorChat,
+        texto: this.textoChat,
+        posicion: this.posicion,
+      }
+      
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
